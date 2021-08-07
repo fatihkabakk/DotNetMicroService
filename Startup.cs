9ae22a2e1e000b74cbccce2faec142106e132eee
Fatih Kabak <fatihkabak39@hotmail.com>
2021-08-07 04:18:31 +0300

    7.8.21 Added Async Weather API Call

--- Startup.cs
+++ Startup.cs
@@ -1,14 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace HelloDotnet5
@@ -32,6 +26,8 @@ namespace HelloDotnet5
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "HelloDotnet5", Version = "v1" });
             });
+
+            services.AddHttpClient<WeatherClient>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
